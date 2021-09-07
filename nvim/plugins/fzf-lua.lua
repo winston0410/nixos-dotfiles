5125d8ff2df83be57d69534e826b4fcc9847f3a9
kghugo <hugosum.dev@protonmail.com>
2021-09-07 17:11:58 +0100

    Add more keybinding

--- nvim/plugins/fzf-lua.lua
+++ nvim/plugins/fzf-lua.lua
@@ -8,27 +8,63 @@ local function init(use)
 		config = function()
 			local actions = require("fzf-lua.actions")
 			require("fzf-lua").setup({
+				winopts = {
+					win_height = 1,
+					win_width = 1,
+				},
 				fzf_layout = "reverse-list",
 				files = {
+					prompt = "Fd❯ ",
 					cmd = "fd",
 					git_icons = true, -- show git icons?
 					file_icons = true, -- show file icons?
 					color_icons = true, -- colorize file|git icons
 				},
 				grep = {
-					prompt = "",
+					prompt = "Rg❯ ",
 					actions = {
 						["Enter"] = actions.file_edit,
 					},
 				},
 			})
 
-			for _, mode in ipairs({ "n", "v", "i" }) do
+			for _, mode in ipairs({ "n", "v" }) do
 				vim.api.nvim_set_keymap(mode, ",m", "<cmd>lua require('fzf-lua').files()<cr>", {
 					silent = true,
 					noremap = true,
 				})
+
+				vim.api.nvim_set_keymap(mode, ",pm", "<cmd>lua require('fzf-lua').files_resume()<cr>", {
+					silent = true,
+					noremap = true,
+				})
+
 				vim.api.nvim_set_keymap(mode, ",g", "<cmd>lua require('fzf-lua').live_grep()<cr>", {
+					silent = true,
+					noremap = true,
+				})
+
+				vim.api.nvim_set_keymap(mode, ",pg", "<cmd>lua require('fzf-lua').live_grep_resume()<cr>", {
+					silent = true,
+					noremap = true,
+				})
+
+				vim.api.nvim_set_keymap(mode, ",a", "<cmd>lua require('fzf-lua').lsp_code_actions()<cr>", {
+					silent = true,
+					noremap = true,
+				})
+
+				vim.api.nvim_set_keymap(mode, ",a", "<cmd>lua require('fzf-lua').lsp_code_actions()<cr>", {
+					silent = true,
+					noremap = true,
+				})
+
+				vim.api.nvim_set_keymap(mode, ",s", "<cmd>lua require('fzf-lua').lsp_document_symbols()<cr>", {
+					silent = true,
+					noremap = true,
+				})
+
+				vim.api.nvim_set_keymap(mode, ",d", "<cmd>lua require('fzf-lua').lsp_document_diagnostics()<cr>", {
 					silent = true,
 					noremap = true,
 				})
