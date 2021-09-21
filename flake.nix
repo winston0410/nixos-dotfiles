3e9e6a8165624660f952fead0304133824a32d2b
kghugo <hugosum.dev@protonmail.com>
2021-09-21 21:38:17 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,54 +3,25 @@
 
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs = { nixpkgs.follows = "nixpkgs"; };
-    };
-    dotfiles-manager = {
-      url = "github:winston0410/dotfiles-manager/master";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-      };
-    };
     universal = {
       url = "github:winston0410/universal-dotfiles/master";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-        dotfiles-manager.follows = "dotfiles-manager";
-      };
-    };
-    eww = {
-      url = "github:elkowar/eww/master";
       inputs = { nixpkgs.follows = "nixpkgs"; };
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, dotfiles-manager, universal, eww, ... }:
+  outputs = inputs:
     let system = "x86_64-linux";
     in {
-      overlay = final: prev: {
-        "eww" = eww.packages.eww;
-      };
-      
-      nixosModule = { pkgs, ... }: {
-        nixpkgs.overlays = [ self.overlay ];
-        environment.systemPackages = [ pkgs.eww ];
-      };
-
       nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
+        nixos = (inputs.universal.profiles.nixos {
+          extraModules = ([
             ./hardware-configuration.nix
-            home-manager.nixosModules.home-manager
-            (universal.profiles.nixos.macbook2017 "hugosum")
-          ];
-        };
+            (inputs.universal.profiles.user.dev { } "hugosum")
+          ]);
+        });
       };
-      devShell.${system} =
-        (import ./shell.nix) { pkgs = nixpkgs.legacyPackages.${system}; };
+      devShell.${system} = (import ./shell.nix) {
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      };
     };
 }
